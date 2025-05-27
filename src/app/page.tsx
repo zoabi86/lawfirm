1803ddf270aca90fa2128e39d78d69ad3280c035
zoabi86 <muhamed.zoabi@gmail.com>
2025-05-27 12:19:11 +0300

    modified profile photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,24 @@ import Services from "@/components/Services";
 import About from "@/components/About";
 import ContactForm from "@/components/ContactForm";
 import Footer from "@/components/Footer";
+import FadeInWhenVisible from "@/components/FadeInWhenVisible";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col font-sans text-gray-800">
       <Navbar />
-      <Hero />
-      <Services />
-      <About />
-      <ContactForm />
+      <FadeInWhenVisible>
+        <Hero />
+      </FadeInWhenVisible>
+      <FadeInWhenVisible delay={0.1}>
+        <Services />
+      </FadeInWhenVisible>
+      <FadeInWhenVisible delay={0.2}>
+        <About />
+      </FadeInWhenVisible>
+      <FadeInWhenVisible delay={0.3}>
+        <ContactForm />
+      </FadeInWhenVisible>
       <Footer />
     </div>
   );
